57f870d3992e7b4a948e1a2ef45238c73d50dbb7
AgusWayer <aguswira246@gmail.com>
2026-04-26 22:13:32 +0800

    Login Page

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import '../utils/validators.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // form key
+  final _formKey = GlobalKey<FormState>();
+
+  // controller
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  // state
+  bool isLoading = false;
+  bool isPasswordVisible = false;
+  String errorMessage = '';
+
+  // Mock Login
+  void login() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+      errorMessage = '';
+    });
+
+    await Future.delayed(const Duration(seconds: 2));
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (email == 'admin@test.com' && password == 'Admin123') {
+      if (!mounted) return;
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Login Berhasil")));
+
+      Navigator.pushNamed(context, '/dashboard', arguments: 'Admin');
+    } else {
+      setState(() {
+        errorMessage = "Email atau password salah";
+      });
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(errorMessage)));
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    @override
+    void dispose() {
+      _emailController.dispose();
+      _passwordController.dispose();
+      super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.lock,
+                      size: 80,
+                      color: Colors.blue,
+                    ),
+                    
+                    const SizedBox(height: 16,),
+                    
+                    const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+
+                    const SizedBox(height: 32,),
+
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          // email
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.email)
+                            ),
+                            validator: Validators.validateEmail,
+                          ),
+
+                          const SizedBox(height: 16,),
+
+                          // password
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: !isPasswordVisible,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: const OutlineInputBorder(),
+                              prefixIcon: const Icon(Icons.lock),
+                              suffixIcon: IconButton(
+                                icon : Icon(
+                                  isPasswordVisible ? Icons.visibility : Icons.visibility_off
+                                ),
+                                onPressed: (){
+                                  setState(() {
+                                    isPasswordVisible = !isPasswordVisible;
+                                  });
+                                }, 
+                                )
+                            ),
+                            validator: Validators.validatePassword,
+                          ),
+
+                          const SizedBox(height: 24,),
+
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: isLoading ? null : login, 
+                              child: isLoading ? const SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                  color: Colors.white,
+                                ),
+                              ) : const Text("Login")
+                            ),
+                          ),
+
+                          const SizedBox(height: 12,),
+
+                          // forgot pw
+
+                          TextButton(
+                            onPressed: (){
+                              Navigator.pushNamed(context, '/forgot-password')
+                            }, 
+                            child: const Text('Lupa Password')
+                          )
+
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+          ),
+      )
+    }
+  }
+}
